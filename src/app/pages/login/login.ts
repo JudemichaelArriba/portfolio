32909272c70cbb920e052390c6772c6d46bc9a21
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-04-02 00:07:22 +0800

    style: Enhance style of login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+// admin-login.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../services/theme.service/theme.service';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
+  selector: 'app-admin-login',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
+  currentTheme: 'dark' | 'light' = 'dark';
 
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit(): void {
+    this.currentTheme = this.themeService.initTheme();
+  }
+
+  toggleTheme(): void {
+    this.currentTheme = this.themeService.toggleTheme();
+  }
 }
