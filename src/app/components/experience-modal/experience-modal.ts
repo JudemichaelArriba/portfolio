4c57cd203ccf023d7a9db924c24f9f43b0871c5d
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-04-03 01:53:44 +0800

    feat: Added animation on closing experience modal

--- src/app/components/experience-modal/experience-modal.ts
+++ src/app/components/experience-modal/experience-modal.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Experience } from '../../models/experience.model';
@@ -7,7 +7,8 @@ import { Experience } from '../../models/experience.model';
   selector: 'app-experience-modal',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './experience-modal.html'
+  templateUrl: './experience-modal.html',
+  styleUrl: './experience-modal.css'
 })
 export class ExperienceModal {
   @Input() experience: Partial<Experience> = {};
@@ -15,8 +16,18 @@ export class ExperienceModal {
   @Input() mode: 'add' | 'edit' = 'add';
   @Output() close = new EventEmitter<void>();
   @Output() save = new EventEmitter<Experience>();
+  isClosing = signal(false);
+
 
   onSubmit() {
     this.save.emit(this.experience as Experience);
+  }
+
+  handleClose() {
+    this.isClosing.set(true);
+
+    setTimeout(() => {
+      this.close.emit();
+    }, 200);
   }
 }
